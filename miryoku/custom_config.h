548e02bbd705d571a883c033f105ffb16551884e
trbute <tr@trbute.com>
2025-03-01 12:01:14 -0700

    update unused key values

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -21,9 +21,9 @@ U_NP,              U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U
      	   K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29,            \
      	   N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39             \
 ) 																			 \
-U_NA  	   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp  U_NA  \
+U_NU  	   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp  U_NU  \
 &kp LSHFT  K10  K11  K12  K13  K14  	 K15  K16  K17  K18  K19  &kp  RSHFT \
-U_NA       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp  U_NA  \
+U_NU       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp  U_NU  \
                		 K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3

0344fd58e54872de30704e50ffb8cb8743dfcbcd
trbute <tr@trbute.com>
2025-04-06 21:50:32 -0600

    to qwerty

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,10 +10,10 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 #define MIRYOKU_LAYERMAPPING_CODE MIRYOKU_MAPPING
 
 #define MIRYOKU_LAYER_BASE 																																								 	\
-&kp Q,             &kp W,         &kp F,              &kp P,              &kp B,             &kp J,            &kp L,             &kp U,            &kp Y,           &kp SQT,               \
-U_MT(LGUI, A),     U_MT(LALT, R), U_MT(LCTRL, S),     U_MT(LSHFT, T),     &kp G,             &kp M,            U_MT(LSHFT, N),    U_MT(LCTRL, E),   U_MT(LALT, I),   U_MT(LGUI, O),         \
-U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C,              &kp D,              &kp V,             &kp K,            &kp H,             &kp COMMA,        U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), \
-U_NP,              U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_CODE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP,            U_NP
+&kp Q,             &kp W,         &kp E,              &kp R,              &kp T,             &kp Y,            &kp U,             &kp I,            &kp O,           &kp P,                 \
+U_MT(LGUI, A),     U_MT(LALT, S), U_MT(LCTRL, D),     U_MT(LSHFT, F),     &kp G,             &kp H,            U_MT(LSHFT, J),    U_MT(LCTRL, K),   U_MT(LALT, L),   U_MT(LGUI, SEMI),      \
+U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C,              &kp V,              &kp B,             &kp N,            &kp M,             &kp COMMA,        U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), \
+U_NP,              U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, TAB), U_LT(U_CODE, SPACE), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP,            U_NP
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( 					  	                 \ 
      	   K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09,            \
@@ -21,9 +21,9 @@ U_NP,              U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U
      	   K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29,            \
      	   N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39             \
 ) 																			 \
-U_NU  	   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp  U_NU  \
+&kp TAB    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp  BSPC  \
 &kp LSHFT  K10  K11  K12  K13  K14  	 K15  K16  K17  K18  K19  &kp  RSHFT \
-U_NU       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp  U_NU  \
+&kp LCTRL  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp  RCTRL \
                		 K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3

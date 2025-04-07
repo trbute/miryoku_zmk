82429c59daa70e877adeb7aa06cd445813cf79e8
trbute <tr@trbute.com>
2025-04-06 21:58:15 -0600

    update edges

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -21,9 +21,9 @@ U_NP,              U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, TAB), U_LT(U_C
      	   K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29,            \
      	   N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39             \
 ) 																			 \
-&kp TAB    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp  BSPC  \
-&kp LSHFT  K10  K11  K12  K13  K14  	 K15  K16  K17  K18  K19  &kp  RSHFT \
-&kp LCTRL  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp  RCTRL \
+&kp TAB    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp  BSLH  \
+&kp LALT   K10  K11  K12  K13  K14  	 K15  K16  K17  K18  K19  &kp  SQT   \
+&kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp  RSHFT \
                		 K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
